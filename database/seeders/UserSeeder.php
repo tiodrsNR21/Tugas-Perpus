1464195f580682b855158d362b3beb2b85974e05
tiodrsNR21 <tiodarussalam96@gmail.com>
2023-12-16 13:19:14 +0700

    commit modul 7 dan 8

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::create([
+            'username' => 'administrator',
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+        ]);
+        $user->assignRole('admin');
+
+        $user = User::create([
+            'username' => 'pustakawan',
+            'name' => 'Pustakawan',
+            'email' => 'pustakawan@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+        ]);
+        $user->assignRole('pustakawan');
+
+        $user = User::create([
+            'username' => 'anggota',
+            'name' => 'Anggota',
+            'email' => 'anggota@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password')
+            'created_at' => now(),
+        ]);
+        $user->assignRole('anggota');
     }
 }
